d1a96538e344541d319387dca7cef76975e61003
Grazielle <grazielle.anaia@gmail.com>
2026-03-25 12:59:20 -0400

    added notification-api ms

---
+++ notification-api/src/main/java/com/grazielleanaia/notification_api/config/KafkaConfig.java
@@ -0,0 +1,23 @@
+package com.grazielleanaia.notification_api.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public DefaultErrorHandler errorHandler(KafkaTemplate<String, Object> kafkaTemplate) {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate);
+        return new DefaultErrorHandler(recoverer, new FixedBackOff(3000L, 3));
+    }
+    //attempt1 X
+    //attempt2 X
+    //attempt3 X
+    //Goes to DLQ: task-events.DLT
+}
