ef5417fe6fd4215ec2d4405f49a0a23e5dac9997
Grazielle <grazielle.anaia@gmail.com>
2026-03-27 14:34:27 -0400

    Implement Kafka consumer for notification-api

--- notification-api/src/main/java/com/grazielleanaia/notification_api/business/TaskEventConsumer.java
+++ notification-api/src/main/java/com/grazielleanaia/notification_api/business/TaskEventConsumer.java
@@ -1,28 +1,27 @@
 package com.grazielleanaia.notification_api.business;
 
 import com.grazielleanaia.notification_api.dto.TaskEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
-@Component
-
 public class TaskEventConsumer {
 
+    private final Logger logger = LoggerFactory.getLogger(TaskEventConsumer.class);
     private final EmailService emailService;
 
     public TaskEventConsumer(EmailService emailService) {
         this.emailService = emailService;
     }
 
-    @KafkaListener(topics = "task-created-topic", groupId = "notification-group")
+    @KafkaListener(topics = "task-created-topic", groupId = "notification-product-create-group")
     public void consume(TaskEvent taskEvent) {
-
-        System.out.println("EVENT RECEIVED: " + taskEvent);
-        try{
+        logger.info("Received task event {}", taskEvent.getTaskId());
+        try {
             emailService.sendNotification(taskEvent);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Failed to process event");
         }
     }
--- notification-api/src/main/java/com/grazielleanaia/notification_api/config/KafkaConfig.java
+++ notification-api/src/main/java/com/grazielleanaia/notification_api/config/KafkaConfig.java
@@ -1,23 +1,42 @@
 package com.grazielleanaia.notification_api.config;
 
 
+import com.grazielleanaia.notification_api.dto.TaskEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
 import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
 
     @Bean
-    public DefaultErrorHandler errorHandler(KafkaTemplate<String, Object> kafkaTemplate) {
+    public DefaultErrorHandler errorHandler(KafkaTemplate<String, TaskEvent> kafkaTemplate) {
         DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate);
         return new DefaultErrorHandler(recoverer, new FixedBackOff(3000L, 3));
     }
-    //attempt1 X
-    //attempt2 X
-    //attempt3 X
-    //Goes to DLQ: task-events.DLT
+
+    @Bean
+    public KafkaTemplate<String, TaskEvent> taskEventKafkaTemplate(ProducerFactory<String, TaskEvent> pf) {
+        return new KafkaTemplate<>(pf);
+    }
+
+    @Bean
+    public ProducerFactory<String, TaskEvent> producerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9094");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
 }
