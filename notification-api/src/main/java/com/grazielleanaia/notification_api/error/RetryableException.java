c88ff381fcb0d138c85d5356b90db6284309c690
Grazielle <grazielle.anaia@gmail.com>
2026-03-27 19:57:07 -0400

    exceptions: RetryableException and NotRetryableException

---
+++ notification-api/src/main/java/com/grazielleanaia/notification_api/error/RetryableException.java
@@ -0,0 +1,12 @@
+package com.grazielleanaia.notification_api.error;
+
+public class RetryableException extends RuntimeException {
+
+    public RetryableException(String message) {
+        super(message);
+    }
+
+    public RetryableException(Throwable cause) {
+        super(cause);
+    }
+}
