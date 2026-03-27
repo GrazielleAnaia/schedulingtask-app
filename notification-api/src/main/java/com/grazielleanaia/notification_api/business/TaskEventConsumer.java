32c31cc66a4ed751c73fb51a475c442e3c4258e8
Grazielle <grazielle.anaia@gmail.com>
2026-03-27 19:57:07 -0400

    exceptions: RetryableException and NotRetryableException

--- notification-api/src/main/java/com/grazielleanaia/notification_api/business/TaskEventConsumer.java
+++ notification-api/src/main/java/com/grazielleanaia/notification_api/business/TaskEventConsumer.java
@@ -1,10 +1,15 @@
 package com.grazielleanaia.notification_api.business;
 
 import com.grazielleanaia.notification_api.dto.TaskEvent;
+import com.grazielleanaia.notification_api.error.NotRetryableException;
+import com.grazielleanaia.notification_api.error.RetryableException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
 
 @Service
 public class TaskEventConsumer {
@@ -17,12 +22,20 @@ public class TaskEventConsumer {
     }
 
     @KafkaListener(topics = "task-created-topic", groupId = "notification-product-create-group")
+    @KafkaHandler
     public void consume(TaskEvent taskEvent) {
         logger.info("Received task event {}", taskEvent.getTaskId());
         try {
             emailService.sendNotification(taskEvent);
+        } catch (ResourceAccessException e) {
+            logger.error(e.getMessage());
+            throw new RetryableException("Failed to process event");
+        } catch (HttpServerErrorException e) {
+            logger.error(e.getMessage());
+            throw new NotRetryableException("Failed to process event");
         } catch (Exception e) {
-            throw new RuntimeException("Failed to process event");
+            logger.error(e.getMessage());
+            throw new NotRetryableException("Failed to process event");
         }
     }
 }
