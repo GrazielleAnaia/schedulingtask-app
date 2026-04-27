d833c6680295ff86c64c31ec41a8183480b7698f
Grazielle <grazielle.anaia@gmail.com>
2026-04-27 13:23:58 -0400

    added spring cloud config dependency to gateway api; set SecurityConfig and KeycloakAuthoritiesConverter

--- gateway/src/main/java/com/grazielleanaia/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/grazielleanaia/gateway/SecurityConfig.java
@@ -1,16 +1,20 @@
 package com.grazielleanaia.gateway;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -21,16 +25,26 @@ public class SecurityConfig {
 
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchanges -> exchanges.pathMatchers("/actuator/health", "/actuator/info").permitAll()
+                .authorizeExchange(exchanges -> exchanges.pathMatchers("/actuator/**").permitAll()
                         .pathMatchers(HttpMethod.OPTIONS).permitAll()
+                        .pathMatchers(HttpMethod.POST, "/api/v1/customers").permitAll()
+                        .pathMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .anyExchange().authenticated())
                 .oauth2ResourceServer(oath2 -> oath2.jwt(Customizer.withDefaults()))
                 .build();
     }
 
+    //Uses JWK to discover metadata and the issuer-uri
     @Bean
     ReactiveJwtDecoder jwtDecoder() {
         return ReactiveJwtDecoders.fromIssuerLocation("http://localhost:8443/realms/gateway");
     }
 
+    @Bean
+    Converter<Jwt, Mono<AbstractAuthenticationToken>> jwtAuthenticationConverter() {
+        ReactiveJwtAuthenticationConverter converter = new ReactiveJwtAuthenticationConverter();
+        converter.setPrincipalClaimName("preferred_username");
+        converter.setJwtGrantedAuthoritiesConverter(new KeycloakAuthoritiesConverter());
+        return converter;
+    }
 }
