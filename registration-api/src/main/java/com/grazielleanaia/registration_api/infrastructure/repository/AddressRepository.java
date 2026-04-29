e9c3d712b83fbd416c0c32e20b4c4ffd7459df01
Grazielle <grazielle.anaia@gmail.com>
2026-04-29 11:51:17 -0400

    docker-compose.yml: modified advertised_listeners to localhost; update the yml files of scheduling-api and notification-api

--- registration-api/src/main/java/com/grazielleanaia/registration_api/infrastructure/repository/AddressRepository.java
+++ registration-api/src/main/java/com/grazielleanaia/registration_api/infrastructure/repository/AddressRepository.java
@@ -3,6 +3,8 @@ package com.grazielleanaia.registration_api.infrastructure.repository;
 
 import com.grazielleanaia.registration_api.infrastructure.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +12,17 @@ import java.util.Optional;
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
-    Optional<Address> findByIdAndCustomerId(Long addressId, Long customerId);
+    //Soft deleted
+    @Query("""
+            select a 
+            from Address a
+            join a.customer c
+            where a.id = :addressId
+            and c.email = :email
+            and c.deleted = false
+            """)
+    Optional<Address> findActiveAddressByIdAndCustomerEmail(@Param ("addressId") Long addressId,
+                                                            @Param("email") String email);
+
     Optional<Address> findByIdAndCustomerEmail(Long addressId, String email);
 }
