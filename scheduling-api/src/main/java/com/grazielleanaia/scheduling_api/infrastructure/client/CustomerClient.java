82a9e34f83d936c01d64acf1346d24624f8249b0
Grazielle <grazielle.anaia@gmail.com>
2026-05-11 11:41:02 -0400

    Added spring boot test to CustomerGateway and TaskService classes

--- scheduling-api/src/main/java/com/grazielleanaia/scheduling_api/infrastructure/client/CustomerClient.java
+++ scheduling-api/src/main/java/com/grazielleanaia/scheduling_api/infrastructure/client/CustomerClient.java
@@ -1,14 +1,12 @@
 package com.grazielleanaia.scheduling_api.infrastructure.client;
 
 
-import com.grazielleanaia.scheduling_api.business.dto.CustomerDTO;
 import com.grazielleanaia.scheduling_api.business.dto.CustomerResponseDTO;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 
 //url = "${customer.url}"
 
@@ -16,12 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "REGISTRATION-API", path = "/api/v1")
 public interface CustomerClient {
 
-    @GetMapping(params = "email")
-    CustomerDTO findCustomerByEmail(@RequestParam("email") String email);
-
     @GetMapping("/{customerId}")
     CustomerResponseDTO findCustomerById(@PathVariable("customerId") Long customerId);
-
 
     @GetMapping("/customers/me")
     CustomerResponseDTO getMyProfile(@RequestHeader("X-User-Email") String email);
